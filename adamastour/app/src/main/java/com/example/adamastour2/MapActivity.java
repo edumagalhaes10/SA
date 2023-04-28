4806c030acd9f16a6f35539fc1b24a06f6340b27
edumagalhaes10 <eduardomagalhaes107@gmail.com>
2023-04-28 19:40:31 +0100

    add mapa

---
+++ adamastour/app/src/main/java/com/example/adamastour2/MapActivity.java
@@ -0,0 +1,39 @@
+package com.example.adamastour2;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    GoogleMap gMap;
+    FrameLayout map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        map = findViewById(R.id.map);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        this.gMap = googleMap;
+        LatLng mapBraga = new LatLng(41.5518,-8.4229);
+        this.gMap.addMarker(new MarkerOptions().position(mapBraga).title("Marker in Braga"));
+        this.gMap.moveCamera(CameraUpdateFactory.newLatLng(mapBraga));
+    }
+}
